484b3db2b17a69cabb8afd171eeb12e3e1f29519
Dylan Hiemstra <dylan1337@outlook.com>
2020-05-14 16:47:23 +0200

    Removed HomeViewModel

--- app/src/main/java/nl/hanze/hanzeboard/activities/overview/home/HomeViewModel.java
+++
@@ -1,7 +0,0 @@
-package nl.hanze.hanzeboard.activities.overview.home;
-
-import androidx.lifecycle.ViewModel;
-
-public class HomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
