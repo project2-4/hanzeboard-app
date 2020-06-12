1bdeb9c25c418697b7bcabb4ae47f9394dfd2af5
Roy Voetman <royvoetman@outlook.com>
2020-06-12 22:10:02 +0200

    Added auto refresh detection

--- app/src/main/java/nl/hanze/hanzeboard/api/interceptors/AuthTokenInject.java
+++ app/src/main/java/nl/hanze/hanzeboard/api/interceptors/AuthTokenInject.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import nl.hanze.hanzeboard.R;
 import nl.hanze.hanzeboard.activities.LoginActivity;
@@ -35,12 +36,9 @@ public class AuthTokenInject implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
 
-        /*if(tokensPreferences.getString(context.getString(R.string.key_jwt_token), "INVALID") != null){
-            tokensPreferences
-                    .edit()
-                    .remove(context.getString(R.string.key_jwt_token))
-                    .apply();
-        }*/
+        request.newBuilder()
+                .addHeader("Accept", "application/json")
+                .build();
 
         if (tokensPreferences.contains(context.getString(R.string.key_jwt_token)) && !request.headers().names().contains("Authorization")) {
             String authToken = "Bearer " + tokensPreferences.getString(context.getString(R.string.key_jwt_token), "INVALID");
@@ -54,6 +52,36 @@ public class AuthTokenInject implements Interceptor {
             Log.d(TAG, "Access token not set");
         }
 
-        return chain.proceed(request);
+        Response response = chain.proceed(request);
+
+        if (response.code() == 401) {
+            synchronized (this) {
+                int code = refreshToken() / 100;
+                if(code != 2) {
+                    logout();
+                }
+
+                // Repeat request with new token
+                String newAccessToken = tokensPreferences.getString(context.getString(R.string.key_jwt_token), "INVALID");
+
+                if(newAccessToken != null) {
+                    return chain.proceed(request.newBuilder()
+                            .addHeader("Authorization", newAccessToken)
+                            .build());
+                }
+            }
+        }
+
+        return response;
+    }
+
+    private int refreshToken() {
+        //Refresh token, synchronously, save it, and return result code
+        //you might use retrofit here
+        return 500;
+    }
+
+    private void logout() {
+        //logout your user
     }
 }
